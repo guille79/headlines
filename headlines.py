2fa67eff56aebbc441f7aadaf66ae978ed7fb279
guille_carbajosa <guille_carbajosa@hotmail.com>
2018-10-07 21:24:34 +0200

    weather API inserted

--- headlines.py
+++ headlines.py
@@ -1,4 +1,4 @@
-import feedparser
+import feedparser, json, urllib
 
 from flask import Flask, render_template, request
 
@@ -12,13 +12,29 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route("/")
 def get_news():
-  query = request.args.get("publication")
-  if not query or query.lower() not in RSS_FEEDS:
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
-  else:
+    else:
         publication = query.lower()
-  feed = feedparser.parse(RSS_FEEDS[publication])
-  return render_template("home.html", articles=feed['entries'])
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    weather = get_weather("London,UK")
+    return render_template("home.html", articles=feed['entries'],
+                           weather=weather)
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d33cd1ed1f2a748c81b150554f6ce20d"
+    query = urllib.request.quote(query)
+    url = api_url.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":parsed["weather"][0]["description"],
+                   "temperature":parsed["main"]["temp"],
+                   "city":parsed["name"]
+                  }
+    return weather
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
